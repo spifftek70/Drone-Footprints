da2c674fc1037df976f38a4c6a52c2f3093ac668
dean <spifftek@me.com>
2019-09-05 13:53:43 -0500

    adding own quaternion processing

---
+++ drone_fp/quaternion_process.py
@@ -0,0 +1,47 @@
+import math
+import numpy as np
+
+
+def to_quaternions(roll, pitch, yaw):
+    roll = math.radians(roll)
+    pitch = math.radians(pitch)
+    yaw = math.radians(yaw)
+    cy = math.cos(-yaw * 0.5)
+    sy = math.sin(-yaw * 0.5)
+    cr = math.cos(-roll * 0.5)
+    sr = math.sin(-roll * 0.5)
+    cp = math.cos(pitch * 0.5)
+    sp = math.sin(pitch * 0.5)
+
+    w = cy * cr * cp + sy * sr * sp
+    x = cy * sr * cp - sy * cr * sp
+    y = cy * cr * sp + sy * sr * cp
+    z = sy * cr * cp - cy * sr * sp
+
+
+def to_euler(w, x, y, z):
+    # roll(x - axis rotation)
+    sinr = +2.0 * (w * x + y * z)
+    cosr = +1.0 - 2.0 * (x * x + y * y)
+    roll = math.atan2(sinr, cosr)
+
+    # pitch (y-axis rotation)
+    sinp = +2.0 * (w * y - z * x)
+    if abs(sinp) >= 1:
+        pitch = math.copysign(math.pi / 2, sinp)  # use 90 degrees if out of range
+    else:
+        pitch = math.asin(sinp)
+    # yaw (z-axis rotation)
+    siny = +2.0 * (w * z + x * y)
+    cosy = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(siny, cosy)
+    return roll, pitch, yaw
+
+
+def quaternion_multiply(quaternion1, quaternion0):
+    w0, x0, y0, z0 = quaternion0
+    w1, x1, y1, z1 = quaternion1
+    return np.array([x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+                     -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
+                     x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0
+                     -x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,], dtype=np.float64)
