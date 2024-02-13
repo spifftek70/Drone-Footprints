dcc3ea92699477471a13faff84b827f793038976
dean <spifftek@me.com>
2024-02-13 06:41:30 -0700

    clean up

--- build/lib/src/create_geotiffs.py
+++ build/lib/src/create_geotiffs.py
@@ -41,22 +41,12 @@ def create_gcp_list(coords, ext):
     ext3 = ext[2][0], ext[2][1]  # Assuming this is bottom-left
     ext0 = ext[3][0], ext[3][1]  # Assuming this is bottom-right
 
-    # Modify the order of GCPs assignment here to correct the mirroring
-    gcp_string = '-gcp {} {} {} {} ' \
-                 '-gcp {} {} {} {} ' \
-                 '-gcp {} {} {} {} ' \
-                 '-gcp {} {} {} {}'.format(ext0[0], ext0[1], pt0[0], pt0[1],
-                                           ext1[0], ext1[1], pt1[0], pt1[1],
-                                           ext2[0], ext2[1], pt2[0], pt2[1],
-                                           ext3[0], ext3[1], pt3[0], pt3[1])
-
-    gcp_items = filter(None, gcp_string.split("-gcp"))
-    gcp_list = []
-    for item in gcp_items:
-        pixel, line, x, y = map(float, item.split())
-        z = 0
-        gcp = gdal.GCP(x, y, z, pixel, line)
-        gcp_list.append(gcp)
+    gcp_list = [
+        gdal.GCP(pt0[0], pt0[1], 1, ext2[0], ext2[1]),
+        gdal.GCP(pt1[0], pt1[1], 1, ext3[0], ext3[1]),
+        gdal.GCP(pt2[0], pt2[1], 1, ext0[0], ext0[1]),
+        gdal.GCP(pt3[0], pt3[1], 1, ext1[0], ext1[1])
+    ]
     return gcp_list
 
 
@@ -79,15 +69,18 @@ def warp_image_with_gcp(image_path, output_file, coord_array):
     ds.SetGCPs(gcp_list, srs.ExportToWkt())
     nodata_value = 0
     # Define warp options
-    warp_options = gdal.WarpOptions(dstSRS='EPSG:' + str(4326),
-                                    resampleAlg=gdal.GRA_Bilinear,
+    vrt_ds = gdal.Translate('', ds, format='VRT')
+
+    warp_options = gdal.WarpOptions(dstSRS=wkt,
+                                    # resampleAlg=gdal.GRA_Bilinear,
                                     format='GTiff',
                                     srcNodata=nodata_value,
                                     dstNodata=nodata_value,
                                     creationOptions=['ALPHA=YES'])  # This option adds an alpha band for transparency
 
     # Perform the warp
-    gdal.Warp(output_file, ds, options=warp_options)
+    vrt_ds = gdal.Warp(output_file, vrt_ds, options=warp_options)
 
     # Clean up
     ds = None
+    vrt_ds = None
