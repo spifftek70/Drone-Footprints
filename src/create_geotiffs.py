5b433f31e000a6db525b9f030792a43e688068ab
dean <spifftek@me.com>
2024-02-18 09:07:36 -0700

    update README.md
    Former-commit-id: 07b2412ca105d19c2c1b6a296d68d88b760309d6 [formerly 363bcdc5df7b7e2dc66725f6c90a82eee9a1e019]
    Former-commit-id: 9d4d64d2dda13b4948838a7fd605b503d9181586
    Former-commit-id: 2d043a790bbb677194bfadb1aee4e8a5b77fea8e

--- src/create_geotiffs.py
+++ src/create_geotiffs.py
@@ -3,12 +3,15 @@
 #  __license__ = "AGPL"
 #  __version__ = "1.0"
 
-import numpy as np
-from osgeo import gdal, osr
+from Utils.raster_utils import *
+import os
+from shapely.geometry import Polygon
 from PIL import Image
-
+import numpy as np
+from Utils.raster_utils import warp_image_to_polygon, warp_ds
 
 def set_raster_extents(image_path, dst_utf8_path, coordinate_array):
+    fixed_polygon = Polygon(coordinate_array)
     # Open the image and convert it to a NumPy array
     jpeg_img = Image.open(image_path)
     jpeg_img_array = np.array(jpeg_img)
@@ -20,81 +23,17 @@ def set_raster_extents(image_path, dst_utf8_path, coordinate_array):
         num_bands = 4
     else:
         raise ValueError("Unsupported image format. Image must be RGB or RGBA.")
-
-    height, width = jpeg_img_array.shape[:2]
-
-    # Calculate bounding box from coordinate_array
-    min_x = min(coordinate[0] for coordinate in coordinate_array)
-    max_x = max(coordinate[0] for coordinate in coordinate_array)
-    min_y = min(coordinate[1] for coordinate in coordinate_array)
-    max_y = max(coordinate[1] for coordinate in coordinate_array)
-
-    # Create a GDAL in-memory dataset
-    gdal.DontUseExceptions()
-    driver = gdal.GetDriverByName('MEM')
-    dataset = driver.Create('', width, height, num_bands, gdal.GDT_Byte)  # Number of bands determined by image format
-
-    # Set geotransform and projection
-    pixel_width = (max_x - min_x) / width
-    pixel_height = (max_y - min_y) / height
-    geotransform = (min_x, pixel_width, 0, max_y, 0, -pixel_height)
-    dataset.SetGeoTransform(geotransform)
-
-    srs = osr.SpatialReference()
-    srs.ImportFromEPSG(4326)  # WGS84
-    dataset.SetProjection(srs.ExportToWkt())
-
-    # Write image data to dataset bands
-    for i in range(num_bands):
-        band = dataset.GetRasterBand(i + 1)
-        if num_bands == 3:  # RGB
-            band.WriteArray(jpeg_img_array[:, :, i])
-        elif num_bands == 4:  # RGBA
-            band.WriteArray(jpeg_img_array[:, :, i])
-    rectify_and_warp_to_geotiff(dataset, dst_utf8_path, coordinate_array)
+    rectify_and_warp_to_geotiff(jpeg_img_array, dst_utf8_path, fixed_polygon, coordinate_array)
 
 
-def gdal_dataset_to_numpy_array(dataset):
-    """Convert a GDAL dataset to a NumPy array, handling both RGB and RGBA images."""
-    bands = [dataset.GetRasterBand(i).ReadAsArray() for i in range(1, dataset.RasterCount + 1)]
-    img = np.dstack(bands)
-    return img
+def rectify_and_warp_to_geotiff(jpeg_img_array, dst_utf8_path, fixed_polygon, coordinate_array):
+    polygon_wkt = str(fixed_polygon)
+    georef_image_array = warp_image_to_polygon(jpeg_img_array, fixed_polygon, coordinate_array)
+    # Turn off GDAL warnings
+    os.environ['CPL_LOG'] = '/dev/null'
+    gdal.SetConfigOption('CPL_DEBUG', 'OFF')
+    dsArray = array2ds(georef_image_array, polygon_wkt)
 
+    # gcp_list = create_gcps(coordinate_array, dsArray)
+    warp_ds(dst_utf8_path, dsArray)
 
-def rectify_and_warp_to_geotiff(ds1a, dst_utf8_path, coordinate_array):
-    # Convert GDAL dataset to NumPy array, handling both RGB and RGBA
-    img = gdal_dataset_to_numpy_array(ds1a)
-
-
-    # Log the format of the image
-    num_channels = img.shape[2]
-    height, width = img.shape[:2]
-
-    # Create an in-memory dataset for the transformed image
-    gdal.DontUseExceptions()
-    driver = gdal.GetDriverByName('MEM')
-    dst_ds = driver.Create('', width, height, num_channels, gdal.GDT_Byte)
-
-    for i in range(num_channels):
-        band = dst_ds.GetRasterBand(i + 1)  # Bands are 1-indexed in GDAL
-        band.WriteArray(img[:, :, i])
-        band.SetNoDataValue(255)  # Optionally set no-data value if applicable
-    # Assign GCPs to the dataset with correct geographic coordinates
-    gcp_list = [
-        gdal.GCP(coordinate_array[0][0], coordinate_array[0][1], 0, 0, 0),
-        gdal.GCP(coordinate_array[1][0], coordinate_array[1][1], 0, width, 0),
-        gdal.GCP(coordinate_array[2][0], coordinate_array[2][1], 0, width, height),
-        gdal.GCP(coordinate_array[3][0], coordinate_array[3][1], 0, 0, height),
-    ]
-
-    srs = osr.SpatialReference()
-    srs.ImportFromEPSG(4326)
-    dst_ds.SetGCPs(gcp_list, srs.ExportToWkt())
-
-    vrt_ds = gdal.Translate(dst_utf8_path, dst_ds, format='GTiff', GCPs=gcp_list, outputSRS=srs.ExportToWkt())
-    warp_options = gdal.WarpOptions(format="GTiff",
-                                    dstSRS='EPSG:4326',
-                                    dstNodata=0,
-                                    outputType=gdal.GDT_Byte,
-                                    resampleAlg=gdal.GRA_Bilinear)
-    gdal.Warp(dst_utf8_path, vrt_ds, options=warp_options)
